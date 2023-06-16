0218052bb4c125237de258a547434f75c7a2f645
devCore <alexanderur@gmail.com>
2023-06-15 22:23:30 -0500

    first commit :v

---
+++ portfolio/src/Portfolio.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar } from './components/navbar';
+import { Footer } from './components/footer';
+
+const Portfolio: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default Portfolio;
