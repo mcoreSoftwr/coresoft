dadd769938df29bb72e62c56fcbd2f8be217ec5a
devCore <alexanderur@gmail.com>
2023-06-16 22:19:42 -0500

    router added

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Navbar: React.FC = () => {
   return (
     <div className="w-full bg-white rounded-lg p-4 flex flex-col gap-4 items-center justify-center md:justify-between">
-      <h1 className="uppercase font-semibold cursor-pointer text-xl">
+      <h1 className=" uppercase font-semibold cursor-pointer text-3xl">
         CoreSoft
       </h1>
       <nav className="flex items-center gap-4">
---
+++ src/components/home/Service.tsx
@@ -0,0 +1,6 @@
+export const Service: React.FC = () => {
+    return (
+        <></>
+    );
+
+}
